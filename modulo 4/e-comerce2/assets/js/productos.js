202477dab2d86d920b90c2af44b8c22f3ec5c637
pedrinchixx <120227882+pedrinchixx@users.noreply.github.com>
2023-03-18 12:32:21 -0300

    Add files via upload

---
+++ modulo 4/e-comerce2/assets/js/productos.js
@@ -0,0 +1,164 @@
+let productos = [
+  {
+    id: 1,
+    nombre: "Arroz",
+    descripcion:"El mejor Arroz tucapel grado 2.",
+    precio: 1600,
+    descuento: 100,
+    imagen: "./assets/img/productos/arroz-tucapel.webp",
+    stock: 3,
+  },
+
+  {
+    id: 2,
+    nombre: "Azucar Flor",
+    descripcion: "El mas fino azucar, Azucar flor 500 g, PRECIO 1750.",
+    precio: 1750,
+    descuento: 50,
+    imagen: "./assets/img/productos/azucar-flor.webp",
+    stock: 3,
+  },
+
+  {
+    id: 3,
+    nombre: "Chocolate Milo",
+    descripcion: "El mas rico Chocolate en polvo, sobre chico de 28 gr, PRECIO 700.",
+    precio: 700,
+    descuento: 80,
+    imagen: "./assets/img/productos/Milo-sobresito.webp",
+    stock: 3,
+  },
+
+  {
+    id: 4,
+    nombre: "Crema de leche",
+    descripcion:
+      "Crema de leche nestle de lata, abre facil de 157 gr, PRECIO 1700.",
+    precio: 1700,
+    descuento: 10,
+    imagen: "./assets/img/productos/Crema-leche.webp",
+    stock: 3,
+  },
+
+  {
+    id: 5,
+    nombre: "Coca-Cola 591 ml",
+    descripcion: "Gaseosa Coca-Cola 591 ml. PRECIO 1400",
+    precio: 1400,
+    descuento: 200,
+    imagen: "./assets/img/productos/Coca-cola-591ml.webp",
+    stock: 3,
+  },
+
+  {
+    id: 6,
+    nombre: "Agua cachantun con gas",
+    descripcion: "Agua mineral cachantun con gas 1.6L, Precio 1200",
+    precio: 1200,
+    descuento: 100,
+    imagen: "./assets/img/productos/Agua-cachantun-congas-1.6.webp",
+    stock: 3,
+  },
+
+  {
+    id: 7,
+    nombre: "Coca cola normal 350ml",
+    descripcion: "Gaseosa Coca Cola normal 350 ml, precio 1100",
+    precio: 1100,
+    descuento: 150,
+    imagen: "./assets/img/productos/coca-cola-350ml.webp",
+    stock: 3,
+  },
+
+  {
+    id: 8,
+    nombre: "Nectar manzana 1,5",
+    descripcion: "Jugo Nectar light manzana watts 1,5, Precio 1900.",
+    precio: 1900,
+    descuento: 80,
+    imagen: "./assets/img/productos/watts-manzana-1,5l.webp",
+    stock: 3,
+  },
+
+  {
+    id: 9,
+    nombre: "Cereal chocapic nestle",
+    descripcion: "Bolsita de Cereal chocapic nestle de 30 gr, PRECIO 600.",
+    precio: 600,
+    descuento: 120,
+    imagen: "./assets/img/productos/chocapic-30gr.webp",
+    stock: 3,
+  },
+
+  {
+    id: 10,
+    nombre: "Nestun 250 gr",
+    descripcion: "Cereal nestun de 250 gr, 5 cereales, PRECIO 2300.",
+    precio: 2300,
+    descuento: 110,
+    imagen: "./assets/img/productos/nestun-250g.webp",
+    stock: 3,
+  },
+
+  {
+    id: 11,
+    nombre: "Barra proteina 42g",
+    descripcion:
+      "Barra proteina snack yoghurt berries your goal 42 gr, PRECIO 1000.",
+    precio: 1000,
+    descuento: 90,
+    imagen: "./assets/img/productos/barra de proteina 42g.webp",
+    stock: 3,
+  },
+
+  {
+    id: 12,
+    nombre: "Cereal estrellitas nestle",
+    descripcion: "Cereal estrellitas nestle caja 330 gr,Precio 3500.",
+    precio: 3500,
+    descuento: 80,
+    imagen: "./assets/img/productos/cereal estrellitas.webp",
+    stock: 3,
+  },
+
+  {
+    id: 13,
+    nombre: "Bonobon de leche",
+    descripcion: "Bon o bon leche arcor 15 gr. PECIO 300",
+    precio: 300,
+    descuento: 120,
+    imagen: "./assets/img/productos/bonobon.webp",
+    stock: 3,
+  },
+
+  {
+    id: 14,
+    nombre: "Chocolate costa rolls",
+    descripcion: "Chocolate costa rolls blanco 140 grs, PRECIO 1600.",
+    precio: 1600,
+    descuento: 100,
+    imagen: "./assets/img/productos/costarolls nuts.webp",
+    stock: 3,
+  },
+
+  {
+    id: 15,
+    nombre: "Kit kat 4 fingr",
+    descripcion: "Kit kat 4 fingr milk 41.5 g, PRECIO 1100.",
+    precio: 1100,
+    descuento: 190,
+    imagen: "./assets/img/productos/KITKAT.webp",
+    stock: 3,
+  },
+
+  {
+    id: 16,
+    nombre: "Orly chocolate frutilla",
+    descripcion: "Orly chocolate frutilla ambrosoli 115 gr, PRECIO 1300.",
+    precio: 1300,
+    descuento: 100,
+    imagen: "./assets/img/productos/orlyfrutilla.webp",
+    stock: 3,
+  }
+]
+export {productos};
---
+++ modulo 5/readme.txt
@@ -0,0 +1,9 @@
+# poratfolio_m5base_de_datos
+
+portafolio correspondiente al modulo 5, respecto a la base de datos y consulta en dicha base de datos de una simulacion
+de compra y venta de productos,generacion de boletas, y algunas consultas en especifico
+
+se realizo en pgAdmin 4 v6, se anexo diagrama de las clases entidad relacion sobre el proyecto, tambien anexa repaldo de la base de datos
+
+
+https://github.com/pedrinchixx/poratfolio_m5base_de_datos.git
